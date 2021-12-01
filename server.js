1505e87aef00f58c545e91491d2db2231ff259e7
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-01 15:22:07 -0600

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-
-const { notes } = require("./db/db.json");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const express = require("express");
 
@@ -15,36 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  let results = notes;
-  res.json(results);
-});
-
-app.post("/api/notes", (req, res) => {
-  req.body.id = notes.length.toString();
-
-  const note = createNewNote(req.body, notes);
-
-  res.json(note);
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public.index.html"));
-});
-
-function createNewNote(body, notesArray) {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
-  return note;
-}
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
