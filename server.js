474dac08faab595113e2b344673e80296e21329c
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-01 14:16:03 -0600

    can write to db.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-// const apiRoutes = require('./routes/apiRoutes/index.js');
-// const htmlRoutes = require('./routes/htmlRoutes/index.js'); 
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+
+const { notes } = require("./db/db.json");
 
 const express = require("express");
 
@@ -14,33 +15,32 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public.index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public.index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-}); 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
 
+  const note = createNewNote(req.body, notes);
 
+  res.json(note);
+});
 
-
-
-
-
-
-
-
-
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
