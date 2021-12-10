73d198d0b9c42314cdc7b52ca662c4096b87a71b
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-09 21:41:26 -0600

    fixed event listener error by fixing index.js html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,16 +1,8 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-
 router.get('/notes', (req, res) => {
-    res.sendFile(__dirname, '../../public/notes.html');
-});
-
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 module.exports = router;
